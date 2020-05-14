ef0feb7113997a9eb14d34068a122745002230f8
mymadhead <mymadhead@gmail.com>
2020-05-14 12:39:50 +0300

    Create model answer

---
+++ db/migrate/20200514093925_create_answers.rb
@@ -0,0 +1,10 @@
+class CreateAnswers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :answers do |t|
+      t.string :title
+      t.integer :level
+
+      t.timestamps
+    end
+  end
+end
