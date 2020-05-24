e93ccf15ac0a7d50399d6fcb7688c2089061be01
mymadhead <mymadhead@gmail.com>
2020-05-24 16:05:01 +0300

    add class&instance methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,10 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_and_belongs_to_many :users
+
+  def self.array_name_tests(category)
+    joins('JOIN categories ON categories.id = tests.category_id')
+      .where('categories.name = ?', category)
+      .order(title: :desc).pluck(:title)
+  end
 end
