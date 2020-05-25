e7b186a82262315774612fcc0087b38609c1817f
mymadhead <mymadhead@gmail.com>
2020-05-25 19:48:44 +0300

    Add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :tests_users, dependent: :destroy
+  has_many :tests, through: :tests_users
+
   def list_all_tests(level)
     Test.joins('JOIN tests_users ON tests.id = tests_users.test_id')
         .where(tests: { level: level }, tests_users: { user_id: id })
