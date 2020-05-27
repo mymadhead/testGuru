cdfcbadae19b33fd4185973baec747db91e8eeb4
mymadhead <mymadhead@gmail.com>
2020-05-27 10:38:46 +0300

    some fixes after review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
-  has_many :tests_users, dependent: :delete_all
+  has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users
 
   def list_all_tests(level)
