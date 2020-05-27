fa1f81a828a9208484443e4d068de00aed835cba
mymadhead <mymadhead@gmail.com>
2020-05-27 17:56:00 +0300

    add validates&scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,22 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
-  belongs_to :category, optional: true
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', optional: true
   has_many :questions, dependent: :destroy
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
 
-  def self.array_name_tests(category)
-    joins(category)
-      .where(categories: { name: category })
-      .order(title: :desc).pluck(:title)
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :easy, -> { where(level: (0..1)) }
+  scope :medium, -> { where(level: (2..4)) }
+  scope :advanced, -> { where(level: (5..Float::INFINITY)) }
+
+  scope :from_category, (->(name) { joins(:category).where(categories: { name: name }).order(title: :desc).pluck })
+
+  def self.title_from_category(name)
+    from_category(name).pluck(:title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_094632) do
+ActiveRecord::Schema.define(version: 2020_05_27_142924) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_094632) do
     t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "tests_users", force: :cascade do |t|
