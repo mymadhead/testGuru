ec3be7d545d1421416967d8523d145ec5959ea4b
mymadhead <mymadhead@gmail.com>
2020-05-29 20:23:01 +0300

    Add methods to controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,48 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: i%[index new create]
+  before_action :find_question, only: i%[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with :rescue_with_question_not_found
+
+  def index
+    @questions = @test.questions
+    render plain: @questions.inspect
+  end
+
   def new
+    @question = Question.new
+  end
+
+  def show
+    render plain: @question.body
+  end
+
+  def create
+    @question = @test.questions.new(post_params)
+    @question.save ? redirect_to @test : render new
+  end
+
+  def destroy
+    @test = @question.test
+    @question.destroy
+    redirect_to @test
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found!'
+  end
+
+  def post_params
+    params.require(:question).permit(:body)
   end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,22 @@
 class TestsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound, with: rescue_with_test_not_found
+
+  def index
+    render plain: Test.all.select(:id, :title).inspect
+  end
+
   def show
+    render 'show'
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Test not found!'
   end
 end
