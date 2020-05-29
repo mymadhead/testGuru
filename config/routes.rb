5a42049df84d060abc0948f6dace9a00aa539bee
mymadhead <mymadhead@gmail.com>
2020-05-29 19:35:54 +0300

    Add controllers&routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tests/show'
+  get 'questions/new'
+
+  resources :tests do
+    resources :questions, shallow: true
+  end
 end
