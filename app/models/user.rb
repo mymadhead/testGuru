56bdfdbeea7db612fd7f6fb7f8389d607fe6821e
mymadhead <mymadhead@gmail.com>
2020-06-05 16:50:15 +0300

    add migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+  has_many :test_passages
+  has_many :tests, through: :tests_passages
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
-  has_many :tests_users, dependent: :destroy
-  has_many :tests, through: :tests_users
 
   validates :email, presence: true
   def list_all_tests(level)
