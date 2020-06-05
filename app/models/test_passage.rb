0ad48d7bd0605dcdd49002d35afa1c01bf7ffb93
mymadhead <mymadhead@gmail.com>
2020-06-05 20:09:05 +0300

    add methods to controllers

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,9 +5,46 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_first_question, on: :create
 
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answer?(answer_ids)
+    save!
+  end
+
+  def completed?
+    current_question.nil?
+  end
+
+  def success_rate
+    ((correct_questions.to_f / test.questions.count) * 100).round
+  end
+
+  def successful?
+    success_rate >= 85
+  end
+
+  def question_count
+    test.questions.order(:id).where('id <= :current', current: current_question.id).count
+  end
+
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_current_question
+    self.current_question = next_question
+  end
+
+  def next_question
+    if current_question.nil?
+      test.questions.order(:id).first
+    else
+      test.questions.order(:id).where('id > :current', current: current_question.id).first
+    end
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == Array(answer_ids).map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
   end
 end
