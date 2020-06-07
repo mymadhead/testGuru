ea320f545e74a4c17f22907a14aa917f3fe50e92
mymadhead <mymadhead@gmail.com>
2020-06-07 20:27:33 +0300

    ready to review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user,
+                :logged?,
+                :log_out
+
+  private
+
+  def authenticate_user!
+    session[:path] = request.path
+    return redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please' unless current_user
+
+    cookies[:email] = current_user.email
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged?
+    current_user.present?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
+  before_save { self.email = email.downcase }
+  validates :name, presence: true
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
-  validates :email, presence: true
+  has_secure_password
+
+  validates :password, length: { minimum: 6 }
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
