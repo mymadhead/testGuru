72288ff0e2dd121afd1fecf68d73b2b40c2ee823
mymadhead <mymadhead@gmail.com>
2020-06-29 22:33:04 +0300

    edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "untitled_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: testguru - mymadhead.herokuapp.com }
+  config.action_mailer.default_url_options = { host: 'testguru - mymadhead.herokuapp.com' }
   config.action_mailer.delivery_method = :imap
   config.action_mailer.smtp_settings = {
     address: 'imap.yandex.ru',
