37d5b2a4d9f3290c06fb3d011c69a922319badf8
mymadhead <mymadhead@gmail.com>
2020-07-05 21:54:22 +0300

    trying to fix feedback#5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://testguru-mymadhead.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
