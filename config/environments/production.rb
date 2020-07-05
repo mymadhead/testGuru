ada749075d7423c61340a0032006d98918baa057
mymadhead <mymadhead@gmail.com>
2020-07-05 18:41:23 +0300

    adding smtp credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'testguru-mymadhead.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mailgun.org',
+    address: 'smtp.sendgrid.net',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
