47d2629a43a8a12e394dacf1ec5e43c4bbbabb2b
mymadhead <mymadhead@gmail.com>
2020-07-05 22:13:06 +0300

    trying to fix feedback#6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,12 +61,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "untitled_production"
 
-  config.action_mailer.default_url_options = { host: 'https://testguru-mymadhead.herokuapp.com' }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'testguru-mymadhead.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
