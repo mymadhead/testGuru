282e5b040c4ffe7ad25dcf116c5ce344a2a6b155
mymadhead <mymadhead@gmail.com>
2020-07-05 21:51:43 +0300

    trying to fix feedback#3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,14 +63,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'testguru-mymadhead.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: 'smtp.sendgrid.net',
-      port: 587,
-      domain: 'sendgrid.com',
-      user_name: ENV['SMTP_USERNAME'],
-      password: ENV['SMTP_PASSWORD'],
-      authentication: 'login',
-      enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
