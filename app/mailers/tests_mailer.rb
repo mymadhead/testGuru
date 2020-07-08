c175527de2fe5fed47374e11ab486a988ad68aa2
mymadhead <mymadhead@gmail.com>
2020-07-08 22:36:58 +0300

    add fixes

--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class TestsMailer < ApplicationMailer
-  def completed_test
-
+  def completed_test(test_passage)
+    @user = test_passage.user
+    @test = test_passage.test
 
     mail to: @user.email, subject: 'Test Guru test completed!'
   end
